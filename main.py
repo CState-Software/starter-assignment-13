9ed4ecb38565a18de434e6b26d8ecfd3f9c0224c
ljo1030 <162507071+ljo1030@users.noreply.github.com>
2026-04-02 12:05:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# Assignment 13 Starter Code
 
+
+# Assignment 13 Starter Code
 def main():
-    print("Start your program here")
+    # TODO: Create your own Student object
+    # TODO: Create a classmate guess Student object
+    # TODO (Bonus): Add user input for a third student
+
+    pass
 
 
 if __name__ == "__main__":
